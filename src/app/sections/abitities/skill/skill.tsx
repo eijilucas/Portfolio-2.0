cfefaa86051600babb6522bac07cd211c24fecd7
Lucas Eiji <104649020+eijilucas@users.noreply.github.com>
2023-10-14 15:51:10 -0300

    fix

---
+++ src/app/sections/abitities/skill/skill.tsx
@@ -0,0 +1,25 @@
+
+interface SkillProps {
+    title: string,
+    description: string,
+    svg: string;
+}
+
+export function Skill(props : SkillProps) {
+
+    return (
+        <div className="skill-section">
+            <div className="skill-text">
+                <div className="skill-title">
+                    <h3>{props.title}</h3>
+                </div>
+                <div className="skill-description">
+                    <p>{props.description}</p>
+                </div>
+            </div>
+            <div className="skill-svg">
+                <img src={props.svg} alt="" />
+            </div>
+        </div>
+    )
+}
